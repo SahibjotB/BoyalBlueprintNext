b801d46d5426ade93830ce1568e46d7e0e25cca1
Sanjot Boyal <sboyal2@uwo.ca>
2026-03-23 03:04:31 -0700

    Creating orchestrator chat service to handle chat flow and intent service that handles determining user query intent with LLM calls.

---
+++ src/lib/services/ai/intentService.ts
@@ -0,0 +1,48 @@
+/* Call this with user query to build system prompt to identify intent */
+
+import { IntentResult } from "@/lib/types/chat";
+import { generateOutput } from "./llmService";
+
+export async function identifyIntent(userQuery:string): Promise<IntentResult> {
+
+    // Instructions for how the AI should take the user query and work with it
+    const systemPrompt = 
+   `You are an intent classification system for a real estate application. Your task is to analyze user queries and classify them into one of the following intents: "property_search", "real_estate", or "other".
+    - property_search: This intent is for users who are looking for properties to buy or rent. They may ask about available listings, property details, prices, locations. I want to purchase, rent. I am looking for a property in this city under this price etc.
+    - real_estate: This intent is for users who are interested in general information about real estate, such as market trends, investment advice, mortgage information, budgets, downpayments, etc.
+    - other: This intent is for queries that do not fit into the above categories
+
+    When you receive a user query, analyze the content and determine the most appropriate intent. Provide a confidence score for your classification, which should be a number between 0 and 1, indicating how confident you are in your classification.
+
+    return JSON: 
+    {
+        "intent": property_search | real_estate | other,
+        "confidence": number (0 to 1)
+    }
+    `;
+
+    // Define the expected schema for the LLM response to ensure we get structured data back that we can work with
+    const intentResultSchema = {
+        name: "intent_classification",
+        schema: {
+            type: "object",
+            properties: {
+                intent: {
+                    type: "string",
+                    enum: ["property_search", "real_estate", "other"]
+                },
+                confidence: {
+                    type: "number",
+                    minimum: 0,
+                    maximum: 1
+                }
+            },
+            required: ["intent", "confidence"],
+            additionalProperties: false
+        }
+    }
+    
+    // call llm service with system prompt and user query, specify structured output with schema for expected return format
+    const response = await generateOutput<IntentResult>({ systemPrompt, userPrompt: userQuery, schema: intentResultSchema });
+    return response;
+}
