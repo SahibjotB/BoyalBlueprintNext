50f29afad5d2772eeb1f3047154f8774358ece8e
Sanjot Boyal <sboyal2@uwo.ca>
2026-06-14 20:41:27 -0700

    Removed all other attributes strictly sticking to price and city as main MLS query ones. Can adjust to include other values later.

--- src/lib/data/propertyAttributes.ts
+++ src/lib/data/propertyAttributes.ts
@@ -11,76 +11,76 @@ export const MLS_ATTRIBUTES = [
         name: "ListPrice",
         type: "number",
         description: "The listed price of the property"
-    },
-    {
-        name: "BedroomsTotal",
-        type: "number",
-        description: "Total number of bedrooms in the property",
-    },
-    {
-        name: "BathroomsTotalInteger",
-        type: "number",
-        description: "Total number of bathrooms in the property"
-    },
-    {
-        name: "BuildingAreaTotal",
-        type: "number",
-        description: "square footage of the home"
-    },
-    {
-        name: "ApproximateAge",
-        type: "number",
-        description: "Approximate age of the property"
-    },
-    {
-        name: "AttachedGarageExistance",
-        type: "boolean",
-        description: "Indicates if the property has an attached garage"
-    },
-    {
-        name: "BalconyType",
-        type: "string",
-        description: "Type of balcony available"
-    },
-    {
-        name: "BasementDetails",
-        type: "string",
-        description: "Finished basement, unfinished basement, separate entrance basement"
-
-    },
-    {
-        name: "Cooling",
-        type: "string",
-        description: "Type of cooling system available"
-    },
-    {
-        name: "DirectionFaces",
-        type: "string",
-        description: "The direction the property faces"
-    },
-    {
-        name: "LotDepth",
-        type: "number",
-        description: "Depth of the lot"
-    },
-    {
-        name: "LotWidth",
-        type: "number",
-        description: "Width of the lot"
-    },
-    {
-        name: "ParkingSpaces",
-        type: "number",
-        description: "Number of parking spaces available"
-    },
-    {
-        name: "StreetName",
-        type: "string",
-        description: "Street name of the propery using examples like: street, drive, court, way, trail, grove, crescent, court, ln, lane, ct, avenue, rd, road, parkway, place"
-    },
-    {
-        name: "TaxAnnualAmount",
-        type: "number",
-        description: "Annual tax amount for the property"
     }
+    // {
+    //     name: "BedroomsTotal",
+    //     type: "number",
+    //     description: "Total number of bedrooms in the property",
+    // }
+    // {
+    //     name: "BathroomsTotalInteger",
+    //     type: "number",
+    //     description: "Total number of bathrooms in the property"
+    // },
+    // {
+    //     name: "BuildingAreaTotal",
+    //     type: "number",
+    //     description: "square footage of the home"
+    // },
+    // {
+    //     name: "ApproximateAge",
+    //     type: "number",
+    //     description: "Approximate age of the property"
+    // },
+    // {
+    //     name: "AttachedGarageExistance",
+    //     type: "boolean",
+    //     description: "Indicates if the property has an attached garage"
+    // },
+    // {
+    //     name: "BalconyType",
+    //     type: "string",
+    //     description: "Type of balcony available"
+    // },
+    // {
+    //     name: "BasementDetails",
+    //     type: "string",
+    //     description: "Finished basement, unfinished basement, separate entrance basement"
+
+    // },
+    // {
+    //     name: "Cooling",
+    //     type: "string",
+    //     description: "Type of cooling system available"
+    // },
+    // {
+    //     name: "DirectionFaces",
+    //     type: "string",
+    //     description: "The direction the property faces"
+    // },
+    // {
+    //     name: "LotDepth",
+    //     type: "number",
+    //     description: "Depth of the lot"
+    // },
+    // {
+    //     name: "LotWidth",
+    //     type: "number",
+    //     description: "Width of the lot"
+    // },
+    // {
+    //     name: "ParkingSpaces",
+    //     type: "number",
+    //     description: "Number of parking spaces available"
+    // },
+    // {
+    //     name: "StreetName",
+    //     type: "string",
+    //     description: "Street name of the propery using examples like: street, drive, court, way, trail, grove, crescent, court, ln, lane, ct, avenue, rd, road, parkway, place"
+    // },
+    // {
+    //     name: "TaxAnnualAmount",
+    //     type: "number",
+    //     description: "Annual tax amount for the property"
+    // }
 ]
